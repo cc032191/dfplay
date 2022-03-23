19d0ca188aa916b8c016755f975ff042ba31229c
cc032191 <373841272@qq.com>
2022-03-23 11:00:22 +0800

    deploy

---
+++ js/hmt.js
@@ -0,0 +1,9 @@
+
+var _hmt = _hmt || [];
+(function () {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?3dae1009fc38020dfceaa0bcdf7b902d";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
+
